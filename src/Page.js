6a80cfeba09794cb0940efd048b885aa0acabb54
tusharkr16 <tusharkumar9871@gmail.com>
2024-06-17 23:32:30 +0530

    created pagination for table data

---
+++ src/Page.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Page = ({ currentPage, totalPages, onPageChange }) => {
+    const buttonStyle = {
+        fontSize: '20px',
+        padding: '8px 15px',
+        borderRadius: '5px',
+        backgroundColor: "green",
+        color: "white",
+        cursor: "pointer"
+    };
+    const handlePrevious = () => {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    const handleNext = () => {
+        console.log("next")
+        if (currentPage < totalPages) {
+            onPageChange(currentPage + 1);
+        }
+    };
+    return (
+        <div style={{ display: 'flex', gap: "20px" }}>
+            <button style={buttonStyle} onClick={() => handlePrevious()} disabled={currentPage === 1}>Previous</button>
+            <button style={buttonStyle}>{currentPage}</button>
+            <button style={buttonStyle} onClick={() => handleNext()}>Next</button>
+        </div>
+    )
+}
+
+export default Page
---
+++ src/Pagination.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'; // Import CSS file for styling
+import Page from './Page';
+
+const Pagination = () => {
+    const [details, setDetails] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+            setDetails(response.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const totalPages = Math.ceil(details.length / itemsPerPage);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = details.slice(indexOfFirstItem, indexOfLastItem);
+
+
+    return (
+        <div className="pagination-container">
+            <h1 style={{ display: "flex", justifyContent: "center" }}>Employee Data Table</h1>
+            <table className="data-table">
+                <thead className='heading'>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {currentItems.map((data) => (
+                        <tr key={data.id}>
+                            <td>{data.id}</td>
+                            <td>{data.name}</td>
+                            <td>{data.email}</td>
+                            <td>{data.role}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }} >
+
+                <Page
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={handlePageChange}
+                />
+            </div>
+
+        </div>
+    )
+}
+
+export default Pagination
